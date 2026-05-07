1811b0d6c0cac7ea5eb4a99a455b0d836dcd0c48
diqizho-u <1770515877@qq.com>
2026-05-07 09:58:07 +0800

    1分页查询 2启用/禁用个人账户 3编辑员工

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -19,4 +21,18 @@ public interface EmployeeMapper {
             + "VALUES "
             + "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser},#{status})")
     void insert(Employee employee);
+/*
+* 分页查询
+* */
+
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+   //修改员工状态
+    void update(Employee employee);
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
